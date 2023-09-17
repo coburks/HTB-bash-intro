8d043df3f9732a23986cccebbd83675e314f3c39
Corrie Burks <106447520+coburks@users.noreply.github.com>
2023-09-17 13:35:08 -0500

    Create exercise2.sh

---
+++ exercise2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+domains=("www.inlanefreight.com ftp.inlanefreight.com vpn.inlanefreight.com" www2.inlanefreight.com)
+echo ${domains[1]}
+
+#this is just calling the 2nd index of an array, nothing crazy
